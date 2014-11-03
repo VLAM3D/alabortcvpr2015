96c121f7fa2471c5d17ad23aa2b9d6ad30297fa4
Joan Alabort <ja310@imperial.ac.uk>
2014-11-03 12:31:35 +0000

    Adds pickle support for AAMs and CLMs via serializablecallable

--- alabortcvpr2015/aam/base.py
+++ alabortcvpr2015/aam/base.py
@@ -2,9 +2,11 @@ from __future__ import division
 import abc
 import numpy as np
 
+from serializablecallable import SerializableCallable
+
 from menpo.shape import TriMesh
 
-from alabortcvpr2015.utils import build_reference_frame
+from menpofit.aam.builder import build_reference_frame
 
 
 # Abstract Interface for AAM Objects ------------------------------------------
@@ -12,6 +14,27 @@ from alabortcvpr2015.utils import build_reference_frame
 class AAM(object):
 
     __metaclass__ = abc.ABCMeta
+
+    def __getstate__(self):
+        import menpo.feature as menpo_feature
+        d = self.__dict__.copy()
+
+        features = d.pop('features')
+        if self.pyramid_on_features:
+            # features is a single callable
+            d['features'] = SerializableCallable(features, [menpo_feature])
+        else:
+            # features is a list of callables
+            d['features'] = [SerializableCallable(f, [menpo_feature])
+                             for f in features]
+        return d
+
+    def __setstate__(self, state):
+        try:
+            state['features'] = state['features'].callable
+        except AttributeError:
+            state['features'] = [f.callable for f in state['features']]
+        self.__dict__.update(state)
 
     @property
     def n_levels(self):
--- alabortcvpr2015/clm/base.py
+++ alabortcvpr2015/clm/base.py
@@ -1,5 +1,7 @@
 from __future__ import division
 
+from serializablecallable import SerializableCallable
+
 
 class CLM(object):
 
@@ -17,6 +19,27 @@ class CLM(object):
         self.scales = scales
         self.scale_shapes = scale_shapes
         self.scale_features = scale_features
+
+    def __getstate__(self):
+        import menpo.feature as menpo_feature
+        d = self.__dict__.copy()
+
+        features = d.pop('features')
+        if self.pyramid_on_features:
+            # features is a single callable
+            d['features'] = SerializableCallable(features, [menpo_feature])
+        else:
+            # features is a list of callables
+            d['features'] = [SerializableCallable(f, [menpo_feature])
+                             for f in features]
+        return d
+
+    def __setstate__(self, state):
+        try:
+            state['features'] = state['features'].callable
+        except AttributeError:
+            state['features'] = [f.callable for f in state['features']]
+        self.__dict__.update(state)
 
     @property
     def n_levels(self):
