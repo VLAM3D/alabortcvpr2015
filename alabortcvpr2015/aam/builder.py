8db10bd86fc6f03a139ad86f7e0e468949fac006
Joan Alabort <ja310@imperial.ac.uk>
2015-03-18 14:59:09 +0000

    Simplify AAMBuilder

--- alabortcvpr2015/aam/builder.py
+++ alabortcvpr2015/aam/builder.py
@@ -1,29 +1,25 @@
 from __future__ import division
 import abc
 from copy import deepcopy
-import numpy as np
 
-from menpo.transform import Scale, Translation, GeneralizedProcrustesAnalysis
 from menpo.model import PCAModel
-from menpo.shape import mean_pointcloud
-from menpo.image import Image
-from menpo.visualize import print_dynamic, progress_bar_str
+from menpo.visualize import print_dynamic
 
 from menpofit.transform import DifferentiablePiecewiseAffine
+from menpofit.builder import (
+    normalization_wrt_reference_shape, build_shape_model)
 from menpofit.aam.base import build_reference_frame
 
-from alabortcvpr2015.utils import fsmooth
+from alabortcvpr2015.utils import (
+    compute_features, scale_images, warp_images, extract_patches)
 
 
 class AAMBuilder(object):
 
     def build(self, images, group=None, label=None, verbose=False):
-        # compute reference shape
-        reference_shape = self._compute_reference_shape(images, group, label,
-                                                        verbose)
-        # normalize images
-        images = self._normalize_images(images, group, label, reference_shape,
-                                        verbose)
+        # normalize images and compute reference shape
+        reference_shape, images = normalization_wrt_reference_shape(
+            images, group, label, self.diagonal, verbose=verbose)
 
         # build models at each scale
         if verbose:
@@ -41,19 +37,17 @@ class AAMBuilder(object):
             # obtain image representation
             if j == 0:
                 # compute features at highest level
-                feature_images = self._compute_features(images, level_str,
-                                                        verbose)
+                feature_images = compute_features(images, verbose, level_str)
                 level_images = feature_images
             elif self.scale_features:
                 # scale features at other levels
-                level_images = self._scale_images(feature_images, s,
-                                                  level_str, verbose)
+                level_images = scale_images(feature_images, s, verbose,
+                                            level_str)
             else:
                 # scale images and compute features at other levels
-                scaled_images = self._scale_images(images, s, level_str,
-                                                   verbose)
-                level_images = self._compute_features(scaled_images,
-                                                      level_str, verbose)
+                scaled_images = scale_images(images, s, verbose, level_str)
+                level_images = compute_features(scaled_images, verbose,
+                                                level_str)
 
             # extract potentially rescaled shapes ath highest level
             level_shapes = [i.landmarks[group][label]
@@ -64,7 +58,7 @@ class AAMBuilder(object):
                 # obtain shape model
                 if verbose:
                     print_dynamic('{}Building shape model'.format(level_str))
-                shape_model = self._build_shape_model(
+                shape_model = build_shape_model(
                     level_shapes, self.max_shape_components)
                 # add shape model to the list
                 shape_models.append(shape_model)
@@ -74,8 +68,8 @@ class AAMBuilder(object):
 
             # obtain warped images
             warped_images = self._warp_images(level_images, level_shapes,
-                                              shape_model.mean(), level_str,
-                                              verbose)
+                                              shape_model.mean(), verbose,
+                                              level_str)
 
             # obtain appearance model
             if verbose:
@@ -100,93 +94,6 @@ class AAMBuilder(object):
         aam = self._build_aam(shape_models, appearance_models, reference_shape)
 
         return aam
-
-    def _compute_reference_shape(self, images, group, label, verbose):
-        # the reference_shape is the mean shape of the images' landmarks
-        if verbose:
-            print_dynamic('- Computing reference shape')
-        shapes = [i.landmarks[group][label] for i in images]
-        ref_shape = mean_pointcloud(shapes)
-        # fix the reference_shape's diagonal length if specified
-        if self.diagonal:
-            x, y = ref_shape.range()
-            scale = self.diagonal / np.sqrt(x**2 + y**2)
-            Scale(scale, ref_shape.n_dims).apply_inplace(ref_shape)
-        return ref_shape
-
-    def _normalize_images(self, images, group, label, ref_shape, verbose):
-        # normalize the scaling of all images wrt the reference_shape size
-        norm_images = []
-        for c, i in enumerate(images):
-            if verbose:
-                print_dynamic('- Normalizing images size: {}'.format(
-                    progress_bar_str((c + 1.) / len(images), show_bar=False)))
-            i = i.rescale_to_reference_shape(ref_shape, group=group,
-                                             label=label)
-            if self.sigma:
-                i.pixels = fsmooth(i.pixels, self.sigma)
-            norm_images.append(i)
-        return norm_images
-
-    def _compute_features(self, images, level_str, verbose):
-        feature_images = []
-        for c, i in enumerate(images):
-            if verbose:
-                print_dynamic(
-                    '{}Computing feature space: {}'.format(
-                        level_str, progress_bar_str((c + 1.) / len(images),
-                                                    show_bar=False)))
-            if self.features:
-                i = self.features(i)
-            feature_images.append(i)
-
-        return feature_images
-
-    @classmethod
-    def _scale_images(cls, images, s, level_str, verbose):
-        scaled_images = []
-        for c, i in enumerate(images):
-            if verbose:
-                print_dynamic(
-                    '{}Scaling features: {}'.format(
-                        level_str, progress_bar_str((c + 1.) / len(images),
-                                                    show_bar=False)))
-            scaled_images.append(i.rescale(s))
-        return scaled_images
-
-    @classmethod
-    def _build_shape_model(cls, shapes, max_components):
-        r"""
-        Builds a shape model given a set of shapes.
-
-        Parameters
-        ----------
-        shapes: list of :map:`PointCloud`
-            The set of shapes from which to build the model.
-        max_components: None or int or float
-            Specifies the number of components of the trained shape model.
-            If int, it specifies the exact number of components to be retained.
-            If float, it specifies the percentage of variance to be retained.
-            If None, all the available components are kept (100% of variance).
-
-        Returns
-        -------
-        shape_model: :class:`menpo.model.pca`
-            The PCA shape model.
-        """
-
-        # centralize shapes
-        centered_shapes = [Translation(-s.centre()).apply(s) for s in shapes]
-        # align centralized shape using Procrustes Analysis
-        gpa = GeneralizedProcrustesAnalysis(centered_shapes)
-        aligned_shapes = [s.aligned_source() for s in gpa.transforms]
-        # build shape model
-        shape_model = PCAModel(aligned_shapes)
-        if max_components is not None:
-            # trim shape model if required
-            shape_model.trim_components(max_components)
-
-        return shape_model
 
     @abc.abstractmethod
     def _build_aam(self, shape_models, appearance_models, reference_shape):
@@ -217,25 +124,9 @@ class GlobalAAMBuilder(AAMBuilder):
         return build_reference_frame(mean_shape, boundary=self.boundary,
                                      trilist=self.trilist)
 
-    def _warp_images(self, images, shapes, ref_shape, level_str, verbose):
-        # compute transforms
-        ref_frame = self._build_reference_frame(ref_shape)
-        # warp images to reference frame
-        warped_images = []
-        for c, (i, s) in enumerate(zip(images, shapes)):
-            if verbose:
-                print_dynamic('{}Warping images - {}'.format(
-                    level_str,
-                    progress_bar_str(float(c + 1) / len(images),
-                                     show_bar=False)))
-            # compute transforms
-            t = self.transform(ref_frame.landmarks['source'].lms, s)
-            # warp images
-            warped_i = i.warp_to_mask(ref_frame.mask, t)
-            # attach reference frame landmarks to images
-            warped_i.landmarks['source'] = ref_frame.landmarks['source']
-            warped_images.append(warped_i)
-        return warped_images
+    def _warp_images(self, images, shapes, ref_shape, verbose, level_str):
+        return warp_images(images, shapes, ref_shape, self.transform,
+                           verbose=verbose, level_str=level_str)
 
     def _build_aam(self, shape_models, appearance_models, reference_shape):
         return GlobalAAM(shape_models, appearance_models, reference_shape,
@@ -261,21 +152,9 @@ class PartsAAMBuilder(AAMBuilder):
         self.max_shape_components = max_shape_components
         self.max_appearance_components = max_appearance_components
 
-    def _warp_images(self, images, shapes, _, level_str, verbose):
-
-        # extract parts
-        parts_images = []
-        for c, (i, s) in enumerate(zip(images, shapes)):
-            if verbose:
-                print_dynamic('{}Warping images - {}'.format(
-                    level_str,
-                    progress_bar_str(float(c + 1) / len(images),
-                                     show_bar=False)))
-            parts_image = Image(i.extract_patches(
-                s, patch_size=self.parts_shape, as_single_array=True))
-            parts_images.append(parts_image)
-
-        return parts_images
+    def _warp_images(self, images, shapes, _, verbose, level_str):
+        return extract_patches(images, shapes, self.parts_shape,
+                               verbose=verbose, level_str=level_str)
 
     def _build_aam(self, shape_models, appearance_models, reference_shape):
         return PartsAAM(shape_models, appearance_models, reference_shape,
--- alabortcvpr2015/utils.py
+++ alabortcvpr2015/utils.py
@@ -2,6 +2,11 @@ from __future__ import division
 import cPickle
 from skimage.filter import gaussian_filter
 
+from menpo.image import Image
+from menpo.visualize import print_dynamic, progress_bar_str
+
+from menpofit.aam.base import build_reference_frame
+
 
 def pickle_load(path):
     with open(str(path), 'rb') as f:
@@ -11,6 +16,70 @@ def pickle_load(path):
 def pickle_dump(obj, path):
     with open(str(path), 'wb') as f:
         cPickle.dump(obj, f, protocol=2)
+
+
+def compute_features(images, features, verbose=None, level_str=""):
+    feature_images = []
+    for c, i in enumerate(images):
+        if verbose:
+            print_dynamic(
+                '{}Computing feature space: {}'.format(
+                    level_str, progress_bar_str((c + 1.) / len(images),
+                                                show_bar=False)))
+        i = features(i)
+        feature_images.append(i)
+
+    return feature_images
+
+
+def scale_images(images, scale, verbose=None, level_str=""):
+    scaled_images = []
+    for c, i in enumerate(images):
+        if verbose:
+            print_dynamic(
+                '{}Scaling features: {}'.format(
+                    level_str, progress_bar_str((c + 1.) / len(images),
+                                                show_bar=False)))
+        scaled_images.append(i.rescale(scale))
+    return scaled_images
+
+
+def warp_images(images, shapes, ref_shape, transform, verbose=None,
+                level_str=""):
+    # compute transforms
+    ref_frame = build_reference_frame(ref_shape)
+    # warp images to reference frame
+    warped_images = []
+    for c, (i, s) in enumerate(zip(images, shapes)):
+        if verbose:
+            print_dynamic('{}Warping images - {}'.format(
+                level_str,
+                progress_bar_str(float(c + 1) / len(images),
+                                 show_bar=False)))
+        # compute transforms
+        t = transform(ref_frame.landmarks['source'].lms, s)
+        # warp images
+        warped_i = i.warp_to_mask(ref_frame.mask, t)
+        # attach reference frame landmarks to images
+        warped_i.landmarks['source'] = ref_frame.landmarks['source']
+        warped_images.append(warped_i)
+    return warped_images
+
+
+def extract_patches(images, shapes, parts_shape, verbose=None,
+                level_str=""):
+    # extract parts
+    parts_images = []
+    for c, (i, s) in enumerate(zip(images, shapes)):
+        if verbose:
+            print_dynamic('{}Warping images - {}'.format(
+                level_str,
+                progress_bar_str(float(c + 1) / len(images),
+                                 show_bar=False)))
+        parts_image = Image(i.extract_patches(
+            s, patch_size=parts_shape, as_single_array=True))
+        parts_images.append(parts_image)
+    return parts_images
 
 
 def flatten_out(list_of_lists):
